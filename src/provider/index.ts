b30ca78f5a20c7cede9cfe6591a187e5bce25624
liblaf <30631553+liblaf@users.noreply.github.com>
2024-12-15 17:19:01 +0800

    refactor: reorganize subscription info fetching logic
    Moves subscription info fetching functions to a dedicated file for better modularity and maintainability. This change improves code organization and readability by centralizing related functionality.

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -3,6 +3,7 @@ export {
   fetchSubInfoSafe,
   fetchSubInfoUnsafe,
 } from "./info";
+export type { SubscriptionUserinfo } from "./info";
 export { PROVIDER_OPTIONS_SCHEMA, PROVIDERS_SCHEMA } from "./types";
 export type { ProviderOptions } from "./types";
 export { getProviderUrl } from "./utils";
--- src/provider/info/index.ts
+++ src/provider/info/index.ts
@@ -1,46 +1,6 @@
-import type { ProviderOptions } from "../types";
-import { getProviderUrl } from "../utils";
-import { fetchClashInfo } from "./clash";
-import { fetchJmsInfo } from "./jms";
-import type { SubscriptionUserinfo } from "./types";
-
-export async function fetchProvidersInfo(
-  providers: ProviderOptions[],
-): Promise<SubscriptionUserinfo[]> {
-  const infos: SubscriptionUserinfo[] = await Promise.all(
-    providers.map(fetchSubInfoSafe),
-  );
-  return infos;
-}
-
-export async function fetchSubInfoSafe(
-  provider: ProviderOptions,
-): Promise<SubscriptionUserinfo> {
-  try {
-    return await fetchSubInfoUnsafe(provider);
-  } catch (error) {
-    return {
-      name: provider.name,
-      url: getProviderUrl(provider),
-      error: `${error}`,
-    };
-  }
-}
-
-export async function fetchSubInfoUnsafe(
-  provider: ProviderOptions,
-): Promise<SubscriptionUserinfo> {
-  let info: SubscriptionUserinfo = {
-    name: provider.name,
-    url: getProviderUrl(provider),
-  };
-  if (provider.clash)
-    info = {
-      ...info,
-      ...(await fetchClashInfo(provider.clash.url, provider.clash.ua)),
-    };
-  else if (provider.jms)
-    info = { ...info, ...(await fetchJmsInfo(provider.jms)) };
-  else throw new Error(`Unsupported provider: ${provider.name}`);
-  return info;
-}
+export {
+  fetchProvidersInfo,
+  fetchSubInfoSafe,
+  fetchSubInfoUnsafe,
+} from "./provider";
+export type { SubscriptionUserinfo } from "./types";
