cfa898154b8fd9038518224955b40dfd62aef79b
liblaf <30631553+liblaf@users.noreply.github.com>
2025-02-23 20:26:00 +0800

    chore: update project configuration and dependencies
    - Update `.envrc` to include `node_modules/.bin` in PATH
    - Update CI workflow to use Bun instead of pnpm
    - Refactor `tsup.config.ts` to simplify entry points
    - Update dependency versions and add missing devDependencies
    - Adjust `.prettierignore` and `.gitignore` patterns
    - Remove redundant `biome.json` as it's now managed in `.github/linters/biome.jsonc`
    - Simplify `package.json` scripts and structure
    - Update TypeScript and other peerDependencies
    - Rename `vite.config.mts` to `vite.config.ts`

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: [
-    "src/client/sing-box/index.ts",
-    "src/filter/index.ts",
-    "src/index.ts",
-    "src/info.ts",
-    "src/provider/index.ts",
-    "src/utils/index.ts",
-  ],
+  entry: ["./src/index.ts"],
   minify: true,
   format: ["cjs", "esm"],
   dts: true,
