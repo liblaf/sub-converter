80c31f25f27706f4e213cabffe748b9bcad17353
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-02 15:22:53 +0800

    refactor(sing-box): simplify iOS template generation
    - removes unused `InboundMixed` import and `findInbound` utility
    - eliminates redundant `http_proxy` configuration in `InboundTun`

--- src/client/sing-box/gen/template/ios.ts
+++ src/client/sing-box/gen/template/ios.ts
@@ -1,11 +1,6 @@
-import type {
-  InboundMixed,
-  InboundTun,
-  Singbox,
-} from "@lib/client/sing-box/schema";
+import type { InboundTun, Singbox } from "@lib/client/sing-box/schema";
 import { InboundTag } from "@lib/const";
 import type { GeneratorSingbox, GeneratorSingboxOptions } from "../types";
-import { findInbound } from "../utils";
 import { DEFAULT } from "./default";
 
 export const IOS: GeneratorSingbox = {
@@ -14,20 +9,12 @@ export const IOS: GeneratorSingbox = {
     options: GeneratorSingboxOptions,
   ): Singbox {
     const singbox: Singbox = DEFAULT.generate(providers, options);
-    const inMixed = findInbound(singbox, InboundTag.MIXED) as InboundMixed;
     singbox.inbounds!.push({
       type: "tun",
       tag: InboundTag.TUN,
       address: ["172.18.0.1/30", "fdfe:dcba:9876::1/126"],
       auto_route: true,
       strict_route: true,
-      platform: {
-        http_proxy: {
-          enabled: true,
-          server: "127.0.0.1",
-          server_port: inMixed.listen_port,
-        },
-      },
     } satisfies InboundTun);
     return singbox;
   },
